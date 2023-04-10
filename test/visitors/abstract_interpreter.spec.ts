ae80963f39386b4234c8ddddb996622bcf690913
Nathan Schulzke <nschulzke@amplify.com>
2023-04-10 11:07:55 -0600

    Rearrange tests

--- test/visitors/abstract_interpreter.spec.ts
+++ test/visitors/abstract_interpreter.spec.ts
@@ -28,22 +28,6 @@ describe("abstract interpreter", () => {
     expect(result).toEqual({z: new AbstractNumber(Interval(0, 20))});
   });
 
-  it("warns about an impossible predicate", () => {
-    const warn = sinon.stub(console, "warn");
-    const input = `
-      z: when this.x < 5 10
-         when this.x < 0 0
-         otherwise 20
-    `;
-
-    const result = abstractInterpret(input, {x: new AbstractNumber(Interval(0, 5))});
-    expect(result).toEqual({z: new AbstractNumber(Interval(10, 20))});
-
-    expect(warn.calledOnce).toBe(true);
-    expect(warn.firstCall.args[0]).toBe("When clause predicate `this.x < 0` will never be true at 3:15");
-    warn.restore();
-  });
-
   it("gives an error for a bad predicate", () => {
     const error = sinon.stub(console, "error");
     const input = `
@@ -75,7 +59,23 @@ describe("abstract interpreter", () => {
     expect(error.firstCall.args[0]).toBe("When clause consequent `true` has wrong type at 3:26");
   });
 
+  it("warns about an impossible predicate", () => {
+    const warn = sinon.stub(console, "warn");
+    const input = `
+      z: when this.x < 5 10
+         when this.x < 0 0
+         otherwise 20
+    `;
+
+    const result = abstractInterpret(input, {x: new AbstractNumber(Interval(0, 5))});
+    expect(result).toEqual({z: new AbstractNumber(Interval(10, 20))});
+
+    expect(warn.calledOnce).toBe(true);
+    expect(warn.firstCall.args[0]).toBe("When clause predicate `this.x < 0` will never be true at 3:15");
+    warn.restore();
+  });
+
   afterEach(() => {
     sinon.restore();
-  })
+  });
 });
